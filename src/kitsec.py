e0784464e6beda0f8839f2d495baa4e2985647b6
idrisschebak <idrisschebak@me.com>
2023-03-03 22:50:04 +0100

    add http parameters shuffler

--- src/kitsec.py
+++ src/kitsec.py
@@ -19,7 +19,6 @@ from Wappalyzer import Wappalyzer, WebPage
 #shuffle proxy / port > user agent / headers 
 
 #ignore JAVA warnings on wappalyzer
-
 warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 warnings.filterwarnings("ignore", category=UserWarning, message=".*It looks like you're parsing an XML document using an HTML parser.*")
 warnings.filterwarnings("ignore", message="""Caught 'unbalanced parenthesis at position 119' compiling regex""", category=UserWarning )
