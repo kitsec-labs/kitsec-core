c965e1ef84089365cde62ef4210c2f4588dc0121
idrisschebak <idrisschebak@me.com>
2023-03-14 13:10:44 +0100

    fix entry point

--- core/setup.py
+++ core/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="kitsec",
-    version="0.1.4b",
+    version="0.1.4rc2",
     author="Idriss CHEBAK",
     author_email="idrisschebak@me.com",
     description="A package designed to streamline Ethical hackers workflows.",
