92a78c165cbc484c3a12ce319ac0d72ac708aaff
idrisschebak <84669419+idrisschebak@users.noreply.github.com>
2023-03-15 13:17:34 +0100

    Packagerize (#10)
    * nightshift
    
    * data fix
    
    * clean imports
    
    * delete active
    
    * fresh test
    
    * update programming language setup.py
    
    * update setup.py
    
    * update readme and docker

--- core/setup.py
+++ core/setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
 setup(
     name="kitsec",
-    version="0.1.6rc3",
+    version="0.1.6rc7",
     author="Idriss CHEBAK",
     author_email="idrisschebak@me.com",
     description="A package designed to streamline Ethical hackers workflows.",
@@ -13,13 +11,12 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/kitsec-labs/kitsec-core",
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
     ],
     install_requires=[
         "Pillow",
@@ -28,7 +25,7 @@ setup(
         "python-magic==0.4.27",
         "cve_search==1.0.11",
         "wappalyzer==1.0.0",
-        "ipaddress==1.0.23",
+        "ipaddress==1.0.7",
         "termcolor==2.2.0",
         "requests==2.28.1",
         "paramiko==3.0.0",
